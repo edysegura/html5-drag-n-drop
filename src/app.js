cb692c545102a6f2ec29d1ca62c769cbd4fb7078
Edy Segura <edysegura@gmail.com>
2022-05-19 22:34:05 -0300

    feat: add drag n drop

--- src/app.js
+++ src/app.js
@@ -13,7 +13,31 @@ draggables.forEach((draggable) => {
 dropZones.forEach((dropZone) => {
   dropZone.addEventListener('dragover', (event) => {
     event.preventDefault()
+    const afterElement = getDragAfterElement(dropZone, event.clientY)
     const draggable = document.querySelector('.dragging')
-    dropZone.appendChild(draggable)
+    if (!afterElement) {
+      dropZone.appendChild(draggable)
+    } else {
+      dropZone.insertBefore(draggable, afterElement)
+    }
   })
 })
+
+function getDragAfterElement(container, mousePositionY) {
+  const draggableElements = [
+    ...container.querySelectorAll('.draggable:not(.dragging)'),
+  ]
+
+  return draggableElements.reduce(
+    (closest, element) => {
+      const box = element.getBoundingClientRect()
+      const offset = mousePositionY - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element }
+      } else {
+        return closest
+      }
+    },
+    { offset: Number.NEGATIVE_INFINITY }
+  ).element
+}
