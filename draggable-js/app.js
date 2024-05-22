5547195eb7f224f5d5240be21c3d1c9731a68aff
Edy Segura <edysegura@gmail.com>
2024-05-22 19:08:19 -0300

    feat: add sortable events

--- draggable-js/app.js
+++ draggable-js/app.js
@@ -1,3 +1,4 @@
+// Draggable documentation: https://shopify.github.io/draggable/docs/
 import { Sortable } from 'https://cdn.jsdelivr.net/npm/@shopify/draggable@1.1.3/+esm'
 
 const container = document.querySelector('[data-sortable]')
@@ -6,4 +7,17 @@ const sortable = new Sortable(container, {
   mirror: {
     constrainDimensions: true,
   },
+})
+
+sortable.on('sortable:start', () => {
+  console.log('sortable:start')
+})
+
+sortable.on('sortable:sorted', () => {
+  console.log('sortable:sorted')
+})
+
+sortable.on('sortable:stop', ({ data }) => {
+  console.log('sortable:stop')
+  console.log(data)
 })
