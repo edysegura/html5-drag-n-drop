1e4ded526477f1dd108a1d7620fafe6c0e025b4a
Edy Segura <edysegura@gmail.com>
2024-05-22 16:36:23 -0300

    feat: add new example using Draggable.js

---
+++ draggable-js/app.js
@@ -0,0 +1,10 @@
+import { Sortable } from 'https://cdn.jsdelivr.net/npm/@shopify/draggable@1.1.3/+esm'
+
+const container = document.querySelector('[data-sortable]')
+const sortable = new Sortable(container, {
+  draggable: 'li',
+  mirror: {
+    // appendTo: container,
+    constrainDimensions: true,
+  },
+})
